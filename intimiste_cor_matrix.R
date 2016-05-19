110d4aa4b7b44a7509f497d6bfe17b7da0a0957d
mickaeltemporao <mickael.temporao.1@ulaval.ca>
2016-05-19 10:41:39 -0400

    corr full & bin

--- intimiste_cor_matrix.R
+++ intimiste_cor_matrix.R
@@ -5,7 +5,7 @@
 # Description:  TODO: (write me)
 # Version:      0.0.0.000
 # Created:      2016-05-03 12:54:53
-# Modified:     2016-05-08 17:22:44
+# Modified:     2016-05-19 10:32:33
 # Author:       Mickael Temporão < mickael.temporao.1 at ulaval.ca >
 # ------------------------------------------------------------------------------
 # Copyright (C) 2016 Mickael Temporão
@@ -13,10 +13,17 @@
 # ------------------------------------------------------------------------------
 
 library(dplyr)
+library(mice)
 library(corrplot)
 
-M <- read.csv("../data/ProjetIntimiste-Imputed-Cdn14Pco14_2015-01-22-Latest.csv")
-M <- M %>% select(proudHlthCareBin,
+Data <- read.csv("data/intimiste.csv")
+
+M <- Data %>% select(contains('proud'))
+M <- complete(mice(M))
+names(M) <- gsub('proud','',names(M))
+names(M) <- gsub('.Pco2014','',names(M))
+
+M <- Data %>% select(proudHlthCareBin,
              proudHistoryBin,proudConstitutionBin,proudBeaverBin,
              proudTFoxBin, proudQueenBin, proudCdnFlagBin, proudAnthemBin,
              proudHockeyBin, proudPeacekeepBin, proudArmyBin,
@@ -26,15 +33,15 @@ M <- M %>% select(proudHlthCareBin,
 
 labs <- c("Healthcare", "History", "Constitution", "Beaver", "Terry Fox", "Queen", "Flag", "Antheem", "Hockey", "Peacekeeping",
 "Army", "Groups treatment", "Maple Leaf", "Wilderness", "Tim Hortons", "Arctic", "Charter", "Mounted Police", "First Nations")
-
 names(M) <- labs
+
 mcor <- round(cor(M, use = "pairwise.complete.obs"),1)
 
-png(height=1200, width=1200, pointsize=25, file="~/Desktop/intimiste_cor.png")
+png(height=1200, width=1200, pointsize=25, file="figs/intimiste_cor_full.png")
 # Ordered by first principal.
 corrplot(mcor, method = 'color', type = 'lower',
          addgrid.col='black',
-         order='FPC', tl.cex=0.8, tl.col='black', tl.srt = 25, tl.offset = 0.6,
-         number.cex=0.8, number.digits=2, addshade='positive',
-         addCoef.col='black', cl.lim=c(0,1))
+         order='FPC', tl.cex=0.6, tl.col='black', tl.srt = 25, tl.offset = 0.6,
+         number.cex=0.6, number.digits=1, addshade='positive',
+         addCoef.col='black')
 dev.off()
